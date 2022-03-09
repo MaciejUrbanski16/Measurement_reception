c1b086d3d1a403d59892b91b3d3bc91c94ca2093
Maciej U <urbanskimac@gmail.com>
2022-03-09 19:21:55 +0100

    Basic exchanging beetween plots

--- RemoteDataInterpreter.cpp
+++ RemoteDataInterpreter.cpp
@@ -4,21 +4,42 @@
 
 #include <wx/dcclient.h>
 #include <wx/panel.h>
+#include <wx/ribbon/buttonbar.h>
+#include <wx/notebook.h>
+#include <random>
+#include "mathplot/mathplot.h"
+
 
 #include "boost/algorithm/cxx11/any_of.hpp"
+
+template<class T = std::mt19937, std::size_t N = T::state_size * sizeof(typename T::result_type)>
+T ProperlySeededRandomEngine () {
+    std::random_device source;
+    auto random_data = std::views::iota(std::size_t(), (N - 1) / sizeof(source()) + 1)
+                       | std::views::transform([&](auto){ return source(); });
+    std::seed_seq seeds(std::begin(random_data), std::end(random_data));
+    return T(seeds);
+}
+
+class MySIN : public mpFX
+{
+    double m_freq, m_amp;
+public:
+    MySIN(double freq, double amp) : mpFX( wxT("f(x) = SIN(x)"), mpALIGN_LEFT) { m_freq=freq; m_amp=amp; m_drawOutsideMargins = false; }
+    virtual double GetY( double x ) { return m_amp * sin(x/6.283185/m_freq); }
+    virtual double GetMinY() { return -m_amp; }
+    virtual double GetMaxY() { return  m_amp; }
+};
 
 RemoteDataInterpreter::RemoteDataInterpreter()
         : wxFrame(NULL, wxID_ANY, "Hello World", wxDefaultPosition, wxSize(300, 250)) {
 
-    wxMenu *menuFile = new wxMenu;
-    menuBar = new wxMenuBar();
-    this->SetMenuBar(menuBar);
     wxPoint point(30,50);
-    panel = new wxPanel();
+    panel = new wxPanel(this, wxID_ANY, {310, 0}, {300, 1000},0x00080000 | wxBORDER_RAISED, "PANEL" );
 
     this->Connect(wxEVT_PAINT, wxPaintEventHandler(RemoteDataInterpreter::OnPaint));
 
-
+    wxMenu *menuFile = new wxMenu;
     menuFile->Append(1, "&Hello...\tCtrl-H",
                      "Help string shown in status bar for this menu item");
     menuFile->AppendSeparator();
@@ -28,13 +49,46 @@ RemoteDataInterpreter::RemoteDataInterpreter()
 
     wxImage::AddHandler(new wxPNGHandler);
 
-    panel1 = new wxPanel(this, wxID_ANY);
+    panel1 = new wxPanel(this, wxID_ANY, {0, 0}, {300,1000},0x00080000 | wxBORDER_RAISED, "PANEL");
     wxButton *button;
     button = new wxButton(panel1, 1000, _T("Connect"), wxPoint(10,10), wxDefaultSize, 0);
 
     wxButton *button1;
-    button = new wxButton(panel1, 1001, _T("Connect"), wxPoint(100,10), wxDefaultSize, 0);
+    button = new wxButton(panel, 1001, _T("Connect"), wxPoint(100,10), wxDefaultSize, 0);
     wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
+//
+    xAxisForPositioning = new mpScaleX(wxT("X"), mpALIGN_BOTTOM, true, mpX_NORMAL);
+    yAxisForPositioning = new mpScaleY(wxT("Y"), mpALIGN_LEFT, true);
+
+    timeAxisForVelocity = new mpScaleX("t[ms]", mpALIGN_BOTTOM, true, mpX_TIME);
+    velocityAxis = new mpScaleY("v[m/s]", mpALIGN_LEFT, true);
+
+    wxFont graphFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+    xAxisForPositioning->SetFont(graphFont);
+    yAxisForPositioning->SetFont(graphFont);
+    xAxisForPositioning->SetDrawOutsideMargins(false);
+    yAxisForPositioning->SetDrawOutsideMargins(false);
+
+    timeAxisForVelocity->SetFont((graphFont));
+    velocityAxis->SetFont(graphFont);
+    timeAxisForVelocity->SetDrawOutsideMargins(false);
+    velocityAxis->SetDrawOutsideMargins(false);
+
+    tracePlot = new mpWindow(this, -1, wxPoint(800, 10), wxSize(600, 600), wxSUNKEN_BORDER );
+    tracePlot->SetMargins(30, 30, 30, 30);
+//    tracePlot->AddLayer(xAxisForPositioning );
+//    tracePlot->AddLayer(yAxisForPositioning );
+
+    velocityPlot = new mpWindow(this, -1, wxPoint(800, 800), wxSize(900, 600), wxSUNKEN_BORDER );
+
+    mpFxyVector = new mpFXYVector();
+    mpFxyVector->SetData(xDataToPlot,yDataToPlot);
+    mpFxyVector->SetContinuity(true);
+    wxPen vectorpen(*wxRED, 2, wxSOLID);
+    mpFxyVector->SetPen(vectorpen);
+    mpFxyVector->SetDrawOutsideMargins(false);
+    tracePlot->AddLayer(mpFxyVector);
+    tracePlot->SetMPScrollbars(true);
 
     wxMenuBar *menuBar = new wxMenuBar;
     menuBar->Append(menuFile, "&File");
@@ -120,6 +174,8 @@ void RemoteDataInterpreter::OnPaint(wxPaintEvent & event){
     {
         dc.DrawPoint(i+12,i+25);
     }
+    dc.DrawRectangle({700,100},{300,100});
+    dc.DrawPoint(500,500);
 }
 
 void RemoteDataInterpreter::OnExit(wxCommandEvent& event)
@@ -155,5 +211,44 @@ void RemoteDataInterpreter::establishServerListening() {
 }
 
 void RemoteDataInterpreter::refreshPanel() {
-    //...
+    auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    std::mt19937 mt_rand(seed);
+    int dice_roll{9};
+    this->dice*=2;
+    xDataToPlot.push_back(static_cast<double>(dice));
+    yDataToPlot.push_back(static_cast<double>(dice*2));
+    mpFxyVector->SetData(xDataToPlot,yDataToPlot);
+
+    mpFxyVector->SetDrawOutsideMargins(false);
+    //tracePlot->AddLayer(mpFxyVector);
+
+    tracePlot->SetPos(800.0, 800.0);
+    if(not clicked)
+    {
+        tracePlot->DelLayer(timeAxisForVelocity);
+        tracePlot->DelLayer(velocityAxis);
+        tracePlot->DelLayer(mpFxyVector);
+        mpFxyVector->SetData(xDataToPlot, yDataToPlot);
+        tracePlot->AddLayer(xAxisForPositioning);
+        tracePlot->AddLayer(yAxisForPositioning);
+        tracePlot->AddLayer(mpFxyVector);
+        tracePlot->SetMPScrollbars(true);
+        tracePlot->SetName("TRACE");
+        tracePlot->Fit();
+        clicked = true;
+    } else {
+        tracePlot->DelLayer(xAxisForPositioning);
+        tracePlot->DelLayer(yAxisForPositioning);
+        tracePlot->DelLayer(mpFxyVector);
+        mpFxyVector->SetData(timeSamples, velocityMperS);
+        tracePlot->AddLayer(timeAxisForVelocity);
+        tracePlot->AddLayer(velocityAxis);
+        tracePlot->AddLayer(mpFxyVector);
+        tracePlot->SetMPScrollbars(true);
+        tracePlot->SetName("VELOCITY");
+        tracePlot->Fit();
+        clicked = false;
+        //tracePlot->~mpWindow();
+    }
+    std::cout<<"Added point: "<<dice<<"   "<< dice*2 << std::endl;
 }
--- RemoteDataInterpreter.h
+++ RemoteDataInterpreter.h
@@ -7,6 +7,7 @@
 
 #include <wx/wx.h>
 #include "wx/socket.h"
+#include "mathplot/mathplot.h"
 #include "VelocityCalculator.h"
 
 class PseudoServerContext;
@@ -33,23 +34,52 @@ private:
 
     void OnPaint(wxPaintEvent & event);
 
+    void establishServerListening();
+    void refreshPanel();
+
     void initAccepting();
 
     wxMenuBar *menuBar{nullptr};
     wxSocketServer *sock{nullptr};
     wxPanel *panel{nullptr};
-    wxPanel* panel1{nullptr};
+    wxPanel *panel1{nullptr};
+    mpWindow *actualPlot{nullptr};
+    mpWindow *tracePlot{nullptr};
+    mpWindow *velocityPlot{nullptr};
+    mpWindow *accelerationPlot{nullptr};
+    mpInfoCoords *nfo{nullptr};
+    mpFXYVector *mpFxyVector{nullptr};
+
+    mpScaleX* xAxisForPositioning{nullptr};
+    mpScaleY* yAxisForPositioning{nullptr};
+
+    mpScaleX* timeAxisForVelocity{nullptr};
+    mpScaleY* velocityAxis{nullptr};
+
+   // RemotePositionDrawer remotePositionDrawer;
 
     int numClients{0};
     int numOfMsg{0};
     constexpr static uint16_t portNr{8000u};
 
-    void establishServerListening();
+
 
     float actualAzimutDeg{0.0f};
     VelocityCalculator velocityCalculator;
 
-    void refreshPanel();
+
+    int dice =1;
+    bool clicked{false};
+
+
+    std::vector<double> xDataToPlot{1.2,128.3,13.4,918.5,-234.7};
+    std::vector<double> yDataToPlot{5.5,525.61,53.7,5.8,543.9};
+
+    std::vector<double> timeSamples{0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0};
+    std::vector<double> velocityMperS{67.6, 231.7, 190.0, 428.0, 8.0 ,456.7, 122.4, 78.9 ,22.1, 543.1};
+
+    std::vector<double> timeStamplesForAcceleration{0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0};
+    std::vector<double> accelerationMperS2{67.6, 428.0, 190.0, 428.0, 428.0 ,456.7, 428.0, 428.0 ,428.0, 543.1};
 };
 
 #endif //SERVER_FOR_TRACKER_REMOTEDATAINTERPRETER_H
